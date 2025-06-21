5fdaaac1f96b13e159e3dae5b213c352f538b20c
Gayathri C N <gayathrinandanan@gmail.com>
2025-06-21 21:48:59 +0300

    darts solution for Exercism

---
+++ darts/darts.py
@@ -0,0 +1,11 @@
+def score(x, y):
+    import math
+    distance = math.sqrt(x*x + y*y)
+    if distance <=1:
+        return 10
+    elif distance <= 5:
+        return 5
+    elif distance <= 10:
+        return 1
+    else: 
+        return 0
